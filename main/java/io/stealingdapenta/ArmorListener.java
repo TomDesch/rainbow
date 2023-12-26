db52436fd74a94267a2155d96e31e99d032f3fce
StealingDaPenta <tomdescheemaeker@hotmail.com>
2023-12-26 21:39:04 +0100

    No more stealing armor

--- main/java/io/stealingdapenta/ArmorListener.java
+++ main/java/io/stealingdapenta/ArmorListener.java
@@ -2,18 +2,23 @@ package io.stealingdapenta;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Objects;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.inventory.ClickType;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.inventory.meta.LeatherArmorMeta;
 
 public class ArmorListener implements Listener {
 
@@ -48,13 +53,41 @@ public class ArmorListener implements Listener {
     }
 
     @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        Player player = event.getPlayer();
+        ItemStack itemInHand = player.getInventory()
+                                     .getItemInMainHand();
+
+        if (playersWearingRainbowArmor.contains(player.getName()) && isRainbowArmor(itemInHand) && event.getAction() == Action.RIGHT_CLICK_AIR) {
+            event.setCancelled(true);
+        }
+    }
+
+    private boolean isRainbowArmor(ItemStack item) {
+        return item.getType()
+                   .name()
+                   .startsWith("LEATHER_") && item.hasItemMeta() && item.getItemMeta() instanceof LeatherArmorMeta;
+    }
+
+
+    @EventHandler
     public void inventoryClick(InventoryClickEvent event) {
         Player player = (Player) event.getWhoClicked();
         Inventory clickedInventory = event.getClickedInventory();
 
-        if (clickedInventory != null && clickedInventory.getType() != InventoryType.PLAYER && playersWearingRainbowArmor.contains(player.getName())
-                && event.getRawSlot() >= 5 && event.getRawSlot() <= 8) {
+        if (playersWearingRainbowArmor.contains(player.getName()) && Objects.nonNull(clickedInventory) && clickedInventory.getType() == InventoryType.PLAYER
+                && isArmorSlot(event.getRawSlot()) && isInvalidClickType(event.getClick())) {
             event.setCancelled(true);
         }
+    }
+
+    private boolean isInvalidClickType(ClickType clickType) {
+        return clickType == ClickType.SHIFT_LEFT || clickType == ClickType.SHIFT_RIGHT || clickType == ClickType.DOUBLE_CLICK
+                || clickType == ClickType.NUMBER_KEY;
+    }
+
+
+    private boolean isArmorSlot(int slot) {
+        return slot >= 5 && slot <= 8;
     }
 }
