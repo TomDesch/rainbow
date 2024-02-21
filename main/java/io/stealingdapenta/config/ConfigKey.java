af12e158f6c9dc0ba88108b0d2ac66b60ee2c369
StealingDaPenta <tomdescheemaeker@hotmail.com>
2024-02-21 08:32:58 +0100

    Added Default values to ConfigKey.java

--- main/java/io/stealingdapenta/config/ConfigKey.java
+++ main/java/io/stealingdapenta/config/ConfigKey.java
@@ -1,19 +1,25 @@
 package io.stealingdapenta.config;
 
 public enum ConfigKey {
-    CYCLE_SPEED("cycle-speed"),
-    PLUGIN_RELOADED_MESSAGE("plugin-reloaded-message"),
-    NO_PERMISSION_MESSAGE("no-permission-message");
+    CYCLE_SPEED("cycle-speed", "defaultValue"),
+    PLUGIN_RELOADED_MESSAGE("plugin-reloaded-message", "Rainbow armor plugin reloaded."),
+    NO_PERMISSION_MESSAGE("no-permission-message", "You don't have permission to use this command.");
 
     private final String key;
+    private final String defaultValue;
 
-    ConfigKey(String key) {
+    ConfigKey(String key, String defaultValue) {
         this.key = key;
+        this.defaultValue = defaultValue;
     }
 
 
     public String getKey() {
         return key;
+    }
+
+    public String getDefaultValue() {
+        return defaultValue;
     }
 
     @Override
