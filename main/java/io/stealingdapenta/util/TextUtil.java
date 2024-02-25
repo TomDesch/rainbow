7d40179f396dbe70f5b1a8f86a9e734625044558
StealingDaPenta <tomdescheemaeker@hotmail.com>
2024-02-25 11:33:11 +0100

    Added a tested TEXT_PATTERN to TestUtil

--- main/java/io/stealingdapenta/util/TextUtil.java
+++ main/java/io/stealingdapenta/util/TextUtil.java
@@ -1,6 +1,7 @@
 package io.stealingdapenta.util;
 
 import java.util.Map;
+import java.util.regex.Pattern;
 import net.kyori.adventure.text.format.Style;
 import net.kyori.adventure.text.format.TextDecoration;
 import net.kyori.adventure.text.format.TextDecoration.State;
@@ -10,13 +11,20 @@ public class TextUtil {
     /**
      * All possible formatting options, exhausted list: b s u i o matching bold strikethrough underline italic obfuscated
      */
-    private static final Map<String, Style> FORMAT_CODE_STYLES = Map.of(
-            "b", createStyle(TextDecoration.BOLD),
-            "s", createStyle(TextDecoration.STRIKETHROUGH),
-            "u", createStyle(TextDecoration.UNDERLINED),
-            "i", createStyle(TextDecoration.ITALIC),
-            "o", createStyle(TextDecoration.OBFUSCATED)
-    );
+    private static final Map<String, Style> FORMAT_CODE_STYLES = Map.of("b", createStyle(TextDecoration.BOLD), "s", createStyle(TextDecoration.STRIKETHROUGH),
+                                                                        "u", createStyle(TextDecoration.UNDERLINED), "i", createStyle(TextDecoration.ITALIC),
+                                                                        "o", createStyle(TextDecoration.OBFUSCATED));
+
+
+    /**
+     * This regex is designed to match color codes in the format &(r,g,b) or the style codes &b, &u, &o, &s, or &i.
+     * The backslashes are used to escape parentheses, and capturing groups allow extracting the individual color components.
+     * Examples: &(12,7,222), &(255,255,255), &b, &u, &o, &s, &i
+     * (.*) allows all the text after.
+     */
+    private static final Pattern TEXT_PATTERN = Pattern.compile("&(?:\\((\\d+),(\\d+),(\\d+)\\)|[buosi])(.*)");
+
+
 
     /**
      * Applies formatting styles based on the provided format code.
